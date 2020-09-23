7b81345c02013b2b8bb1ff704ae17b7f4e1f7e4f
mohammed-20 <ma7mmed.n3im@gmail.com>
2020-09-23 19:50:07 +0300

    stupe js file

--- js/index.js
+++ js/index.js
@@ -43,3 +43,119 @@ const siteContent = {
 };
 
 // write your code here
+
+/*************edit***************/
+
+
+// add acontent in nav
+const item1=document.querySelector('body > div > header > nav > a:nth-child(1)');
+item1.textContent=siteContent.nav["nav-item-1"]
+
+const item2=document.querySelector('body > div > header > nav > a:nth-child(2)');
+item2.textContent=siteContent.nav["nav-item-2"];
+
+const item3=document.querySelector('body > div > header > nav > a:nth-child(3)');
+item3.textContent=siteContent.nav["nav-item-3"];
+
+const item4=document.querySelector('body > div > header > nav > a:nth-child(4)');
+item4.textContent=siteContent.nav["nav-item-4"];
+
+const item5=document.querySelector('body > div > header > nav > a:nth-child(5)');
+item5.textContent=siteContent.nav["nav-item-5"];
+
+const item6=document.querySelector('body > div > header > nav > a:nth-child(6)');
+item6.textContent=siteContent.nav["nav-item-6"];
+
+
+
+//add img content in nav
+const img_nav=document.querySelector('#logo-img');
+img_nav.src=siteContent.nav['img-src'];
+
+//add h1 content 
+const h1_cta=document.querySelector('body > div > section.cta > div > h1');
+h1_cta.textContent=siteContent.cta.h1;
+
+// add button content 
+const btn_cta=document.querySelector('body > div > section.cta > div > button');
+btn_cta.textContent=siteContent.cta.button;
+
+//add img content 
+const img_cta= document.querySelector('#cta-img');
+img_cta.src=siteContent.cta['img-src'];
+
+//add content for main top
+
+const h4_features=document.querySelector('body > div > section.main-content > div.top-content > div:nth-child(1) > h4');
+h4_features.textContent=siteContent["main-content"]["features-h4"];
+
+const p_features=document.querySelector('body > div > section.main-content > div.top-content > div:nth-child(1) > p');
+p_features.textContent=siteContent["main-content"]["features-content"];
+
+const h4_about=document.querySelector('body > div > section.main-content > div.top-content > div:nth-child(2) > h4');
+h4_about.textContent=siteContent["main-content"]["about-h4"];
+
+const p_about=document.querySelector('body > div > section.main-content > div.top-content > div:nth-child(2) > p');
+p_about.textContent=siteContent["main-content"]["about-content"];
+
+
+//add content for main middle
+
+// add img content for main middle
+const img_mid=document.querySelector('#middle-img');
+img_mid.src=siteContent["main-content"]["middle-img-src"];
+
+// add bottom text content 
+//#1 services
+const h4_services=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(1) > h4');
+h4_services.textContent=siteContent["main-content"]["services-h4"];
+
+const p_services=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(1) > p');
+p_services.textContent=siteContent["main-content"]["services-content"];
+//#2 product
+const h4_product=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(2) > h4');
+h4_product.textContent=siteContent["main-content"]["product-h4"];
+
+const p_product=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(2) > p');
+p_product.textContent=siteContent["main-content"]["product-content"];
+//#3 vision
+const h4_vision=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(3) > h4');
+h4_vision.textContent=siteContent["main-content"]["vision-h4"];
+
+const p_vision=document.querySelector('body > div > section.main-content > div.bottom-content > div:nth-child(3) > p');
+p_vision.textContent=siteContent["main-content"]["vision-content"];
+
+// add contact content
+const h4_cont=document.querySelector('body > div > section.contact > h4');
+h4_cont.textContent=siteContent.contact["contact-h4"];
+
+const p_address=document.querySelector('body > div > section.contact > p:nth-child(2)');
+p_address.textContent=siteContent.contact.address;
+
+const p_phone=document.querySelector('body > div > section.contact > p:nth-child(3)');
+p_phone.textContent=siteContent.contact.phone;
+
+const p_email=document.querySelector('body > div > section.contact > p:nth-child(4)');
+p_email.textContent=siteContent.contact.email;
+
+/******footer******/
+//add content footer
+
+const p_footer=document.querySelector('body > div > footer > p');
+p_footer.textContent=siteContent.footer.copyright;
+
+// edit color of nav
+const navbar_light=document.querySelector('body > div > header > nav');
+navbar_light.style.backgroundColor="green";
+
+// add item to nav system
+const item7=document.createElement('a');
+item7.textContent= 'News';
+const item8=document.createElement('a');
+item8.textContent='Tools';
+
+const add=document.querySelector('body > div > header > nav');
+add.appendChild(item7);
+add.appendChild(item8);
+
+
